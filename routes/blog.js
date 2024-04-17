d913b9560eedcf39ac578991c068fe25b49b891d
Necip Dirik <necipdirik@gmail.com>
2024-04-17 09:32:58 +0300

    new posts inserted into mongodb collection

--- routes/blog.js
+++ routes/blog.js
@@ -1,21 +1,44 @@
 const express = require('express');
+const mongodb = require('mongodb');
 
 const router = express.Router();
 
+const ObjectId = mongodb.ObjectId;
+
 const db = require('../data/database');
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   res.redirect('/posts');
 });
 
-router.get('/posts', function(req, res) {
+router.get('/posts', function (req, res) {
   res.render('posts-list');
 });
 
-router.get('/new-post', async function(req, res) {
+router.get('/new-post', async function (req, res) {
   const authors = await db.getDb().collection('authors').find().toArray();
   console.log(authors);
-  res.render('create-post', {authors: authors});
+  res.render('create-post', { authors: authors });
+});
+
+router.post('/posts', async function (req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection('authors').findOne({ _id: authorId });;
+
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    author: {
+      id: authorId,
+      name: author.name,
+      email: author.email,
+    },
+  };
+
+  const result = await db.getDb().collection('posts').insertOne(newPost);
+  console.log(result);
+  res.redirect('/posts');
 });
 
 module.exports = router;
